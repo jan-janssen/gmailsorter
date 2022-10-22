1d0d522ab338956565b461714e27ec0acc8073b4
Jan Janssen <jan.janssen@outlook.com>
2022-10-22 12:53:25 -0600

    Restructure commandline

--- pygmailsorter/__main__.py
+++ pygmailsorter/__main__.py
@@ -4,9 +4,14 @@ from pygmailsorter.interface import Gmail
 
 def command_line_parser():
     """
-    Main function primarly used for the command line interface
+    Main function primarily used for the command line interface
     """
     parser = argparse.ArgumentParser(prog="pygmailsorter")
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Configuration Folder e.g. ~/.pygmailsorter",
+    )
     parser.add_argument(
         "-d",
         "--database",
@@ -14,19 +19,21 @@ def command_line_parser():
         help="Update local database.",
     )
     parser.add_argument(
-        "-c",
-        "--config",
-        help="Configuration Folder e.g. ~/.pygmailsorter",
+        "-l",
+        "--label",
+        help="Email label to be filtered with machine learning",
     )
     parser.add_argument(
         "-m",
         "--machinelearning",
-        help="Email label to be filtered with machine learning.",
+        action="store_true",
+        help="Train machine learning models. (requires -l/ --label)",
     )
     parser.add_argument(
-        "-n",
-        "--gmailfilterlabel",
-        help="Email label to be filtered with machine learning from Gmail.",
+        "-p",
+        "--prediction",
+        action="store_true",
+        help="Sort emails based on prediction of machine learning models. (requires -l/ --label)",
     )
     args = parser.parse_args()
     if args.config:
@@ -35,14 +42,13 @@ def command_line_parser():
         gmail = Gmail()
     if args.database:
         gmail.update_database(quick=False)
-    elif args.machinelearning:
-        gmail.update_database(quick=True, label_lst=[args.machinelearning])
-        gmail.filter_label_by_machine_learning(
-            label=args.machinelearning, recalculate=True
-        )
-    elif args.gmailfilterlabel:
-        gmail.update_database(quick=True, label_lst=[args.gmailfilterlabel])
-        gmail.filter_only_new_messages(label=args.gmailfilterlabel, recalculate=True)
+    if args.label:
+        if args.machinelearning:
+            _ = gmail.train_machine_learning_models(label=args.label, recalculate=True)
+        elif args.prediction:
+            gmail.filter_only_new_messages(label=args.label, recalculate=False)
+        else:
+            parser.print_help()
     else:
         parser.print_help()
 

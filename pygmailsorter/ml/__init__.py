2ff0710e2e4753bc461ac85cb6e4aa2e667feaa4
Jan Janssen <jan.janssen@outlook.com>
2022-10-22 18:25:54 -0600

    Restructure machine learning module

---
+++ pygmailsorter/ml/__init__.py
@@ -0,0 +1,3 @@
+from pygmailsorter.ml.encoding import encode_df_for_machine_learning
+from pygmailsorter.ml.database import get_machine_learning_database
+from pygmailsorter.ml.model import fit_machine_learning_models, get_predictions_from_machine_learning_models

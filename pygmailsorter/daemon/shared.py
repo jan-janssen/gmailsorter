ed4bf1218a61f4f067cf37113c0a36339ca1c965
Jan Janßen <janssen@mpie.de>
2023-07-09 09:01:47 -0600

    black formatting

--- pygmailsorter/daemon/shared.py
+++ pygmailsorter/daemon/shared.py
@@ -17,7 +17,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/userinfo.profile",
     "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/gmail.settings.basic",
-    "openid"
+    "openid",
 ]
 
 

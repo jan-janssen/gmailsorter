56ff2adf4c2cc75c8e808ea40ff027dde9831f53
Jan Janßen <janssen@mpie.de>
2023-07-24 19:16:24 -0600

    Rename status from failed to fail

--- gmailsorter/webapp/render.py
+++ gmailsorter/webapp/render.py
@@ -1,7 +1,7 @@
 def color_for_status(status):
     if status == "success":
         return '<span style="color:MediumSeaGreen;">' + status + "</span>"
-    elif status == "failed":
+    elif status == "fail":
         return '<span style="color:Tomato;">' + status + "</span>"
     else:
         return '<span style="color:Orange;">' + status + "</span>"

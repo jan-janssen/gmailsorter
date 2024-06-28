99dfd60a158347b52dbbf59bf34e134117d056fc
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-06-28 19:31:53 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         ],
     },
     install_requires=[
-        "google-api-python-client==2.134.0",
+        "google-api-python-client==2.135.0",
         "google-auth==2.30.0",
         "google-auth-oauthlib==1.2.0",
         "numpy==2.0.0",

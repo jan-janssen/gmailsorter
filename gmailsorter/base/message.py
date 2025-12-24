5a81c1ab06fbd1ab503de09c8a1c124a557f39b7
Jan Janssen <jan-janssen@users.noreply.github.com>
2025-12-24 13:59:06 +0100

    Update message.py

--- gmailsorter/base/message.py
+++ gmailsorter/base/message.py
@@ -13,14 +13,8 @@ def email_date_converter(email_date):
         email_date = " ".join(email_date.split()[:-1])
     if email_date[-1].isalpha():
         email_date = email_date[:-1]
-    if "," in email_date and email_date.count(",") == 1 and not email_date[:3].isalpha():
-        email_date = email_date.replace(",", "").strip()
-    if "." in email_date:
-        email_date = email_date.split(".")[0]
-    if email_date.count(" ") == 5:
-        email_date = " ".join(email_date.split()[:-1])
     if email_date[:3].isalpha() and email_date[-3] != ":" and email_date[-6] == "_":
-        return datetime.strptime(email_date, "%a, %d %b %Y %H:%M:%S")
+        return datetime.strptime(email_date.split(".")[0], "%a, %d %b %Y %H:%M:%S %z")
     elif email_date[:3].isalpha() and email_date[-3] != ":" and "(" in email_date:
         return datetime.strptime(email_date.split(" (")[0], "%a, %d %b %Y %H:%M:%S %z")
     elif email_date[:3].isalpha() and email_date[-3] != ":":
@@ -29,10 +23,6 @@ def email_date_converter(email_date):
         return datetime.strptime(email_date, "%a, %d %b %Y %H:%M:%S")
     elif email_date.count("-") == 2:
         return datetime.strptime(email_date, "%d-%m-%Y")
-    elif email_date.count(" ") == 4 and email_date.count(":") == 2:
-        return datetime.strptime(email_date, "%d %b %Y %H:%M:%S")
-    elif email_date.count(" ") == 5 and email_date.count(":") == 2:
-        return datetime.strptime(email_date, "%d %b %Y %H:%M:%S %z")
     else:
         return datetime.strptime(email_date, "%d %b %Y %H:%M:%S %z")
 

b84362da85af09cd59391e761fbf7421fa6c9229
Jan Janßen <janssen@mpie.de>
2026-04-11 21:25:46 +0200

    black fixes

--- gmailsorter/ml/encoding.py
+++ gmailsorter/ml/encoding.py
@@ -146,7 +146,13 @@ def _single_entry_email_df(red_lst, value_lst):
     return np.array(
         [
             [
-                1 if email is not None and isinstance(email, str) and red_entry in email else 0
+                (
+                    1
+                    if email is not None
+                    and isinstance(email, str)
+                    and red_entry in email
+                    else 0
+                )
                 for red_entry in red_lst
                 if red_entry is not None
             ]

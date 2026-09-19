0513541d08b0632f4a1018dd487307ce8e0a3c47
Jan Janssen <jan-janssen@users.noreply.github.com>
2026-09-19 14:26:56 +0200

    Update __all__ to exclude 'Imap'
    Removed 'Imap' from the __all__ list in __init__.py.

--- src/gmailsorter/__init__.py
+++ src/gmailsorter/__init__.py
@@ -3,4 +3,4 @@ from gmailsorter.local import Gmail, load_client_secrets_file
 from . import _version
 
 __version__: str = _version.__version__
-__all__ = ["Gmail", "Imap", "load_client_secrets_file"]
+__all__ = ["Gmail", "load_client_secrets_file"]
